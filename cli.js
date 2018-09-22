09fc3705d41f0a7ac4866d86b2cbcd4be89ebda3
Andrew Barrette <barrette.andrew.dev@gmail.com>
2018-09-22 16:17:55 -0500

    hardcode an ignore and add dir recursion

--- cli.js
+++ cli.js
@@ -3,7 +3,7 @@ const fs = require('fs')
 const path = require('path')
 const mkdirp = require('mkdirp-promise')
 const fsPromises = fs.promises
-const { map, filter, forEach, compose, replace } = require('ramda')
+const { map, not, reduce, filter, forEach, compose, replace, test } = require('ramda')
 
 // Helpers
 const isDir = filepath => fs.lstatSync(filepath).isDirectory()
@@ -11,21 +11,20 @@ const readFile = filepath => fsPromises.readFile(filepath, { encoding: 'utf8'})
   .then(body => ({ filepath, body }))
 const puke = err => console.error(err, '🤮')
 const thumbsUp = filepath => console.log(filepath, '👍')
+const ignoreFiles = filepath => !test(/ignoredir/, filepath)
 
-// The star of the whole show
-const resolve = replace(/^\s+/mg, '')
+const getFilesFromArgs = args => {
+  const filtered = filter(ignoreFiles, [].concat(args))
+  console.log('FILTERED', filtered)
 
-// Execute
-const run = finishingMove => {
-  const files = filter(x => !isDir(x), process.argv.slice(2))
-  console.log('Harmonizing ✌️...\n\n', files, '\n')
-
-  const mapResolve = map(({ filepath, body }) => ({ filepath, body: resolve(body) }))
-  const resolveAndFinish = compose(forEach(finishingMove), mapResolve)
- 
-  return Promise
-    .all(map(readFile, files))
-    .then(resolveAndFinish)
+  return reduce((accumulator, current) => {
+    if (isDir(current)) {
+      const files = fs.readdirSync(current).map(file => `${current}/${file}`)
+      return accumulator.concat(files)
+    } else {
+      return accumulator.concat(current)
+    }
+  }, [], filtered)
 }
 
 const writeToDist = ({ filepath, body }) => {
@@ -36,5 +35,27 @@ const writeToDist = ({ filepath, body }) => {
     .catch(puke)
 }
 
+// Messaging
+const logBegin = files => console.log('Harmonizing ✌️...\n\n', files, '\n')
+const logFinish = () => console.log('\n\nThank you, hero. You have made the world a better place. 🦋 🌺 🐈')
+
+// The star of the whole show
+const resolve = replace(/^\s+/mg, '')
+
+// Main
+const run = finishingMove => {
+  // const files = filter(x => !isDir(x), )
+  const files = getFilesFromArgs(process.argv.slice(2))
+  logBegin(files)
+
+  const mapResolve = map(({ filepath, body }) => ({ filepath, body: resolve(body) }))
+  const resolveAndFinish = compose(forEach(finishingMove), mapResolve)
+ 
+  return Promise
+    .all(map(readFile, files))
+    .then(resolveAndFinish)
+}
+
+// Execute
 run(writeToDist)
-  .then(() => console.log('\n\nThank you, hero. You have made the world a better place. 🦋 🌺 🐈'))
+  .then(logFinish)
