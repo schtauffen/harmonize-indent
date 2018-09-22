898547d3e1e22d6d4c15f639c8a1f953e914ca3b
Andrew Barrette <barrette.andrew.dev@gmail.com>
2018-09-22 11:54:27 -0500

    read files in and resolve them

--- cli.js
+++ cli.js
@@ -1,13 +1,25 @@
 #!/usr/bin/env node
+const fs = require('fs')
+const fsPromises = fs.promises
 
-const argv = process.argv.slice(2)
+// Helpers
+const isDir = path => fs.lstatSync(path).isDirectory()
+const readFile = file => fsPromises.readFile(file, { encoding: 'utf8'})
+const resolve = str => str.replace(/^\s+/mg, '')
 
-const str = ` \t  my string
-  \t\t\tanother string
-`
 
-function resolve(str) {
-  return str.replace(/^\s+/mg, '')
+// Execute
+const run = finishingMove => {
+  const files = process.argv.slice(2)
+ 
+  Promise
+    .all(files.map(readFile))
+    .then(
+      strings => {
+        strings.map(resolve).forEach(finishingMove)
+      }
+    )
 }
 
-console.log(resolve(str))
+run(a => { console.log(a) })
+
